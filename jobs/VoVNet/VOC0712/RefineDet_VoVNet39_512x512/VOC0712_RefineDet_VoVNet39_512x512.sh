fd893805c1b0ea2d06893485b7cec59a246c0fc0
youngwanlee <youngwan88@gmail.com>
2019-11-19 17:16:39 +0900

    remove jobs

--- jobs/VoVNet/VOC0712/RefineDet_VoVNet39_512x512/VOC0712_RefineDet_VoVNet39_512x512.sh
+++
@@ -1,5 +0,0 @@
-cd /home/ywlee/VoVNet-RefineDet
-./build/tools/caffe train \
---solver="models/VoVNet/VOC0712/RefineDet_VoVNet39_512x512/solver.prototxt" \
---weights="models/ImageNet/VoVNet/VoVNet39_ImageNet_Pretrained.caffemodel" \
---gpu 0,1,2,3 2>&1 | tee jobs/VoVNet/VOC0712/RefineDet_VoVNet39_512x512/VOC0712_RefineDet_VoVNet39_512x512.log
